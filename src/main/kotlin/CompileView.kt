5bde1c59d7d866ab27063418c03be5a19d804098
Andreas Thaler <andreas.thaler@quickbirdstudios.com>
2021-02-14 19:09:33 +0100

    added button onClick action

--- src/main/kotlin/CompileView.kt
+++ src/main/kotlin/CompileView.kt
@@ -1,11 +1,15 @@
+import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Orientation
 import tornadofx.*
 
 class CompileView : View() {
+    private val model = ViewModel()
+    private val cCode = model.bind { SimpleStringProperty() }
+
     override val root = form {
         fieldset(labelPosition = Orientation.VERTICAL) {
             label("C Code")
-            textarea {
+            textarea(cCode) {
                 prefWidth = 800.0
                 prefRowCount = 7
             }
@@ -13,18 +17,30 @@ class CompileView : View() {
             button("Translate to RISC-V") {
                 isDefaultButton = true
                 useMaxWidth = true
+                action {
+                    val sourceCode = cCode.value
+                    if (sourceCode.isNotEmpty()) sendCompileRequest(cCode.value)
+                }
             }
             label("")
             label("RISC-V Assembly")
             textarea {
                 prefRowCount = 7
+                isEditable = false
             }
             label("Error Stream")
             textarea {
                 prefRowCount = 3
+                isEditable = false
             }
             label("Compilation Time")
-            textfield()
+            textfield("5700 ms") {
+                isEditable = false
+            }
         }
+    }
+
+    private fun sendCompileRequest(sourceCode: String) {
+
     }
 }
